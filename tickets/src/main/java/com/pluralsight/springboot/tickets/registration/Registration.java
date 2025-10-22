bfbde85c34df875a8cd758e551f41bdc0fc5c79b
Tzouliano Giazai <tzoulianogiazai@gmail.com>
2025-10-22 16:36:48 +0300

    new changes

--- tickets/src/main/java/com/pluralsight/springboot/tickets/registration/Registration.java
+++ tickets/src/main/java/com/pluralsight/springboot/tickets/registration/Registration.java
@@ -2,9 +2,12 @@ package com.pluralsight.springboot.tickets.registration;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-public record Registration(Integer id,
-                           @NotNull(message = "Product is is required") Integer productId,
+@Document("registrations")
+public record Registration(@Id String id,
+                           @NotNull(message = "Product id is required") Integer productId,
                            String ticketCode,
                            @NotBlank(message = "Attendee name is required") String attendeeName) {
 }
